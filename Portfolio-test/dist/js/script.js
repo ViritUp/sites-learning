094f420a17dd5e30a731ea9f38f505aa5b015391
Dmitry <22072003dz2@gmail.com>
2022-02-01 23:47:33 +0200

    chenged portfolio

--- Portfolio-test/dist/js/script.js
+++ Portfolio-test/dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percent = document.querySelectorAll('.skills__statistics-percent');
+      lines = document.querySelectorAll('.skills__statistics-statusbar span'); 
+
+percent.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
