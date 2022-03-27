1f69238c907ccbeae7a76b24f31e7d2bb8674feb
Dmitry <22072003dz2@gmail.com>
2022-03-28 01:16:32 +0300

    Go-Serf - created + first screen

---
+++ go-serf/app/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">',
+        asNavFor: '.slider-dots',
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
+
+  
+});
+
