88ba9ba7c508cc8d8160c3fe3bb9cdf07634deed
Dmitry <22072003dz2@gmail.com>
2022-03-30 02:09:26 +0300

    go-serf: create second section -surf

--- go-serf/app/js/main.js
+++ go-serf/app/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    /*------------------- HEADER-SLIDER ------------------- */
     $('.header__slider').slick({
         infinite: true,
         fade: true,
@@ -13,6 +14,31 @@ $(function(){
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
+
+
+    /*------------------- surf-MAP ------------------- */
+    $('.surf-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf__slider',
+        focusOnSelect: true
+    });
+
+
+    /*------------------- surf-SLIDER ------------------- */
+    $('.surf__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        centerMode: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">',
+        asNavFor: '.surf-map',
+    });
+
 
   
 });
