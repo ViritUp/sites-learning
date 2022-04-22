eb3c665cedd10b183a707dd03a16e6429b2600e2
Dmitry <22072003dz2@gmail.com>
2022-04-22 14:37:53 +0300

    TheRoad - final version of project

--- the-road/script.js
+++ the-road/script.js
@@ -7,3 +7,22 @@ buttons.forEach((item) => {
 });
 
 
+
+const COLORS = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+
+document.querySelectorAll('.nav-link').forEach((link, idx) => {
+    link.style.backgroundColor = COLORS[idx];
+});
+
+
+
+const openBtn = document.querySelector('.open-navbar-icon');
+const closeBtn = document.querySelector('.close-navbar-icon');
+const container = document.querySelector('.container');
+
+openBtn.addEventListener('click', () => {
+    container.classList.add('change');
+});
+closeBtn.addEventListener('click', () => {
+    container.classList.remove('change');
+});
