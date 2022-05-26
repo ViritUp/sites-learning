587a764ac2b475837771253c96234be3b1a9d560
Dmitry <22072003dz2@gmail.com>
2022-05-26 14:43:52 +0300

    Assassins hoodie mantle

---
+++ assassins-hoodie-mantle/js/common.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	const showSlider = new Swiper('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev'
+		}
+	});
+
+
+
+	// ускоряем скорость видео (.playbackRate)
+	document.querySelector('video').playbackRate = 2;
+
+
+})
